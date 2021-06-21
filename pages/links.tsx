bbcce7dd6a8345153bc7137e9cd1d59bee499e7f
optydeviocourses <66975293+optydeviocourses@users.noreply.github.com>
2021-06-21 07:12:03 -0300

    Update links.tsx
    Ajustes do domínio

--- pages/links.tsx
+++ pages/links.tsx
@@ -7,7 +7,7 @@ import { BsFillLightningFill } from 'react-icons/bs'
 
 import { Container, Button, ButtonsContainer } from '../styles/links/styles'
 
-const blogName = process.env.BLOG_NAME || 'optydev-io.tech'
+const blogName = process.env.BLOG_NAME || 'optydev-io.com'
 
 const blogContexts =
   process.env.BLOG_CONTEXT ||
